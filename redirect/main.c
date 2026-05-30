3af250ccf954ca52ec2504edce7dd93d3fa144c0
krn1pnc <krn1pnc@outlook.com>
2026-05-30 14:49:02 +0800

    1

--- redirect/main.c
+++ redirect/main.c
@@ -1,23 +1,74 @@
+#define _GNU_SOURCE
+
 #include <arpa/inet.h>
+#include <errno.h>
 #include <linux/netfilter_ipv4.h>
-#include <netinet/in.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/epoll.h>
-#include <sys/socket.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
-#include <unistd.h>
 
 const char *LOG_DIR = "./log";
 const int DIRECT_MARK = 0x0416;
+const size_t BUF_SIZE = 128;
 
 static volatile sig_atomic_t should_stop = 0;
 void signal_handler_stop(int signum) { should_stop = 1; }
+
+void format_addr(const struct sockaddr_in *addr, char *res, size_t len) {
+    snprintf(res, len, "%s:%d", inet_ntoa(addr->sin_addr), ntohs(addr->sin_port));
+}
+
+void log_info(const char *fmt, ...) {
+    va_list(args);
+    va_start(args, fmt);
+
+    char msg[BUF_SIZE];
+    vsnprintf(msg, sizeof(msg), fmt, args);
+    printf("[INFO] %s\n", msg);
+}
+
+void log_info_pid(const char *fmt, ...) {
+    va_list(args);
+    va_start(args, fmt);
+
+    char msg[BUF_SIZE];
+    vsnprintf(msg, sizeof(msg), fmt, args);
+    printf("[INFO pid=%d] %s\n", getpid(), msg);
+}
+
+void log_error(const char *fmt, ...) {
+    va_list(args);
+    va_start(args, fmt);
+
+    char msg[BUF_SIZE];
+    vsnprintf(msg, sizeof(msg), fmt, args);
+    printf("[ERROR] %s\n", msg);
+}
+
+void log_error_info(const char *fmt, ...) {
+    va_list(args);
+    va_start(args, fmt);
+
+    char msg[BUF_SIZE];
+    vsnprintf(msg, sizeof(msg), fmt, args);
+    printf("[ERROR name=%s desc=\"%s\"] %s\n", strerrorname_np(errno), strerrordesc_np(errno), msg);
+}
+
+void log_error_info_pid(const char *fmt, ...) {
+    va_list(args);
+    va_start(args, fmt);
+
+    char msg[BUF_SIZE];
+    vsnprintf(msg, sizeof(msg), fmt, args);
+    printf("[ERROR pid=%d name=%s desc=\"%s\"] %s\n", getpid(), strerrorname_np(errno), strerrordesc_np(errno), msg);
+}
+
 int set_signal_handler() {
     struct sigaction sa;
     sa.sa_handler = signal_handler_stop;
@@ -30,14 +81,13 @@ int set_signal_handler() {
     return 0;
 }
 
-void format_addr(const struct sockaddr_in *addr, char *res, size_t len) {
-    snprintf(res, len, "%s:%d", inet_ntoa(addr->sin_addr), ntohs(addr->sin_port));
-}
-
+bool _nftable_created;
 int setup_nftables(int port) {
     int ret;
 
     if ((ret = system("nft add table ip chocomint")) != 0) return ret;
+
+    _nftable_created = true;
 
     if ((ret = system("nft 'add chain ip chocomint output { type nat hook output priority mangle + 1; policy accept; }'")) != 0) return ret;
 
@@ -49,154 +99,137 @@ int setup_nftables(int port) {
 
     return 0;
 }
-
-void cleanup_nftables() { system("nft delete table ip chocomint"); }
+void cleanup_nftables() {
+    if (_nftable_created) system("nft delete table ip chocomint");
+}
 
 int handle_connection(int client_fd, const struct sockaddr_in *src_addr, const struct sockaddr_in *dst_addr) {
-    int connid;
-    char errmsg[64];
-    char src_addr_str[64], dst_addr_str[64];
+    char src_addr_str[BUF_SIZE], dst_addr_str[BUF_SIZE];
     int server_fd;
-    char log_filename[256];
+    char log_filename[BUF_SIZE];
     FILE *log_fp;
     int epoll_fd;
     struct epoll_event ev;
     ssize_t up_bytes, down_bytes;
 
-    connid = rand();
-
     format_addr(src_addr, src_addr_str, sizeof(src_addr_str));
     format_addr(dst_addr, dst_addr_str, sizeof(dst_addr_str));
-    printf("[%d] New connection: %s -> %s\n", connid, src_addr_str, dst_addr_str);
+    log_info_pid("new connection %s -> %s", src_addr_str, dst_addr_str);
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        sprintf(errmsg, "[%d] socket() failed", connid);
-        perror(errmsg);
-
-        exit(1);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        log_error_info_pid("socket() failed");
+        goto EXIT;
     }
 
     if (setsockopt(server_fd, SOL_SOCKET, SO_MARK, &DIRECT_MARK, sizeof(DIRECT_MARK)) < 0) {
-        sprintf(errmsg, "[%d] setsockopt() failed", connid);
-        perror(errmsg);
-
-        close(server_fd);
-        exit(1);
+        log_error_info_pid("setsockopt() failed");
+        goto EXIT_SERVER_FD;
     }
 
     if (connect(server_fd, (const struct sockaddr *)dst_addr, sizeof(*dst_addr))) {
-        sprintf(errmsg, "[%d] connect() failed", connid);
-        perror(errmsg);
-
-        close(server_fd);
-        exit(1);
+        log_error_info_pid("connect() failed");
+        goto EXIT_SERVER_FD;
     }
 
-    sprintf(log_filename, "%s/%d_%s_to_%s.log", LOG_DIR, connid, src_addr_str, dst_addr_str);
-
-    mkdir(LOG_DIR, 0755);
-
+    if (mkdir(LOG_DIR, 0755) < 0) {
+        if (errno == EEXIST) log_info_pid("log dir already existed");
+        else log_error_info_pid("mkdir() failed");
+    }
+    sprintf(log_filename, "%s/%ld_%s_to_%s.log", LOG_DIR, time(NULL), src_addr_str, dst_addr_str);
     log_fp = fopen(log_filename, "wb");
     if (!log_fp) {
-        sprintf(errmsg, "[%d] fopen() failed", connid);
-        perror(errmsg);
-
-        close(server_fd);
-        exit(1);
+        log_error_info_pid("fopen() failed");
+        goto EXIT_SERVER_FD;
     }
-    if ((epoll_fd = epoll_create1(0)) < 0) {
-        sprintf(errmsg, "[%d] epoll_create1() failed", connid);
-        perror(errmsg);
 
-        fclose(log_fp);
-        close(server_fd);
-        exit(1);
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        log_error_info_pid("epoll_create1() failed");
+        goto EXIT_LOG_FP;
     }
 
     ev.events = EPOLLIN;
     ev.data.fd = client_fd;
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) < 0) {
-        sprintf(errmsg, "[%d], epoll_ctl() failed", connid);
-        perror(errmsg);
-
-        close(epoll_fd);
-        fclose(log_fp);
-        close(server_fd);
-        exit(1);
+        log_error_info_pid("failed to add client epoll event");
+        goto EXIT_EPOLL_FD;
     }
 
     ev.events = EPOLLIN;
     ev.data.fd = server_fd;
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) < 0) {
-        sprintf(errmsg, "[%d] epoll_ctl() failed", connid);
-        perror(errmsg);
-
-        close(epoll_fd);
-        fclose(log_fp);
-        close(server_fd);
-        exit(1);
+        log_error_info_pid("failed to add server epoll event");
+        goto EXIT_EPOLL_FD;
     }
 
     while (true) {
         struct epoll_event e[2];
         int ne;
-        if ((ne = epoll_wait(epoll_fd, e, 2, -1)) < 0) {
-            sprintf(errmsg, "[%d] epoll_wait() failed", connid);
-            perror(errmsg);
-            break;
+        char buf[4096];
+
+        ne = epoll_wait(epoll_fd, e, 2, -1);
+        if (ne < 0) {
+            log_error_info_pid("epoll_wait() failed");
+            goto EXIT_EPOLL;
         }
 
-        bool end_listen = false;
-
-        char buf[4096];
         for (int i = 0; i < ne; i++) {
+            ssize_t n, m;
             int cfd = e[i].data.fd;
+
             if (cfd == client_fd) {
-                ssize_t n = read(client_fd, buf, sizeof(buf));
+                n = read(client_fd, buf, sizeof(buf));
                 if (n <= 0) {
-                    printf("[%d] read() failed on client\n", connid);
-                    end_listen = true;
-                    break;
+                    if (n < 0) log_error_info_pid("read() failed on client");
+                    goto EXIT_EPOLL;
                 }
                 up_bytes += n;
-                ssize_t m = write(server_fd, buf, n);
+
+                m = write(server_fd, buf, n);
                 if (m != n) {
-                    printf("[%d] write() failed on server\n", connid);
-                    end_listen = true;
-                    break;
+                    log_error_info_pid("write() failed on server");
+                    goto EXIT_EPOLL;
                 }
+
                 fprintf(log_fp, ">>> %lu bytes\n", n);
                 fwrite(buf, sizeof(char), n, log_fp);
                 fputc('\n', log_fp);
             } else if (cfd == server_fd) {
-                ssize_t n = read(server_fd, buf, sizeof(buf));
+                n = read(server_fd, buf, sizeof(buf));
                 if (n <= 0) {
-                    printf("[%d] read() failed on server\n", connid);
-                    end_listen = true;
-                    break;
+                    if (n < 0) log_error_info_pid("read() failed on server");
+                    goto EXIT_EPOLL;
                 }
                 down_bytes += n;
-                ssize_t m = write(client_fd, buf, n);
+
+                m = write(client_fd, buf, n);
                 if (m != n) {
-                    printf("[%d] write() failed on client\n", connid);
-                    end_listen = true;
-                    break;
+                    log_error_info_pid("write() failed on client");
+                    goto EXIT_EPOLL;
                 }
+
                 fprintf(log_fp, "<<< %lu bytes\n", n);
                 fwrite(buf, sizeof(char), n, log_fp);
                 fputc('\n', log_fp);
             }
         }
 
-        if (end_listen) break;
+        continue;
+    EXIT_EPOLL:
+        break;
     }
 
-    printf("[%d] %s -> %s >>>%lu <<<%lu\n", connid, src_addr_str, dst_addr_str, up_bytes, down_bytes);
+    log_info_pid("connection %s -> %s closed >>>%lu <<<%lu", src_addr_str, dst_addr_str, up_bytes, down_bytes);
 
+EXIT_EPOLL_FD:
     close(epoll_fd);
+EXIT_LOG_FP:
     fclose(log_fp);
+EXIT_SERVER_FD:
     close(server_fd);
-    return 0;
+EXIT:
+    exit(0);
 }
 
 int main(int argc, char **argv) {
@@ -207,11 +240,11 @@ int main(int argc, char **argv) {
     socklen_t listen_addr_len = sizeof(listen_addr);
     char listen_addr_str[32];
     int listen_port;
-    int ret;
 
-    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("socker() failed: %s\n", strerrordesc_np(errno)), ret = 1;
-        goto FREE;
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
+        log_error_info("socket() failed");
+        goto EXIT;
     }
 
     memset(&listen_addr, 0, sizeof(listen_addr));
@@ -219,28 +252,28 @@ int main(int argc, char **argv) {
     listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     listen_addr.sin_port = htonl(0);
     if (bind(listen_fd, (struct sockaddr *)&listen_addr, sizeof(listen_addr)) < 0) {
-        perror("bind() failed"), ret = 1;
-        goto FREE_LISTEN_FD;
+        log_error_info("bind() failed");
+        goto EXIT_LISTEN_FD;
     }
 
     if (listen(listen_fd, 16) < 0) {
-        perror("listen() failed"), ret = 1;
-        goto FREE_LISTEN_FD;
+        log_error_info("listen() failed");
+        goto EXIT_LISTEN_FD;
     }
 
     if (getsockname(listen_fd, (struct sockaddr *)&listen_addr, &listen_addr_len) < 0) {
-        perror("getsockname() failed"), ret = 1;
-        goto FREE_LISTEN_FD;
+        log_error_info("getsockname() failed");
+        goto EXIT_LISTEN_FD;
     }
 
     format_addr(&listen_addr, listen_addr_str, sizeof(listen_addr_str));
-    printf("listen on %s\n", listen_addr_str);
+    log_info("listen on %s", listen_addr_str);
 
     listen_port = ntohs(listen_addr.sin_port);
 
-    if ((ret = setup_nftables(listen_port)) != 0) {
-        printf("failed to setup nftables\n");
-        goto FREE_NFTABLES;
+    if (setup_nftables(listen_port) != 0) {
+        log_error("failed to setup nftables");
+        goto EXIT_NFTABLES;
     }
 
     while (!should_stop) {
@@ -251,36 +284,36 @@ int main(int argc, char **argv) {
         socklen_t server_addr_len = sizeof(server_addr);
         pid_t pid;
 
-        if ((client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
-            perror("accept() failed");
+        client_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_fd < 0) {
+            log_error_info("accept() failed");
             continue;
         }
 
         if (getsockopt(client_fd, SOL_IP, SO_ORIGINAL_DST, &server_addr, &server_addr_len)) {
-            perror("getsockopt() failed");
+            log_error_info("getsockopt() failed");
             continue;
         }
 
         pid = fork();
         if (pid < 0) {
-            perror("fork() failed");
-            goto FREE_CLIENT_FD;
+            log_error_info("fork() failed");
+            goto EXIT_CLIENT_FD;
         }
 
         if (pid == 0) {
             close(listen_fd);
             handle_connection(client_fd, &client_addr, &server_addr);
-            return 0;
         }
 
-    FREE_CLIENT_FD:
+    EXIT_CLIENT_FD:
         close(client_fd);
     }
 
-FREE_NFTABLES:
+EXIT_NFTABLES:
     cleanup_nftables();
-FREE_LISTEN_FD:
+EXIT_LISTEN_FD:
     close(listen_fd);
-FREE:
-    return ret;
+EXIT:
+    return 0;
 }
